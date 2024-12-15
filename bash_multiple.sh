ab4cfc38f393a9618f526dd16101c39f921094d0
Adam Henriksson <x_adahe@berzelius1.nsc.liu.se>
2024-12-15 18:36:21 +0100

    temp 0.05 data

--- bash_multiple.sh
+++ bash_multiple.sh
@@ -2,28 +2,31 @@
 #
 #SBATCH -A Berzelius-2024-336 -p berzelius
 #SBATCH -J ADAMHENR_APR_BENCH
-#SBATCH -t 04:00:00
-#SBATCH -N 1 --gpus-per-node=3 -C "fat"
+#SBATCH -t 8:00:00
+#SBATCH -N 1 --gpus-per-node=2
 
 # Env
 container_name="elleelleaime.sif"
 
 # dataset="HumanEvalJava"
 # dataset_path="humaneval-java"
-# dataset="Defects4J"
-# dataset_path="defects4j"
-dataset="GitBugJava"
-dataset_path="gitbug-java"
+dataset="Defects4J"
+dataset_path="defects4j"
+# dataset="GitBugJava"
+# dataset_path="gitbug-java"
 method="sigonly-instruct"
 sample_model_name="<<sample_model_name>>"
 # patch_strategy="openai-chatcompletion"
 # candidate_model_name="gpt-4o-mini"
 patch_strategy="codellama-instruct"
 candidate_model_name="meta-llama/CodeLlama-7b-Instruct-hf"
-temp="0.8"
+# patch_strategy="google"
+# candidate_model_name="gemini-1.5-flash"
+
+temp="0.05"
 
 samples_path="../APR-benchmark-results/data/${dataset_path}/samples_${dataset}_${method}_.jsonl"
-kwargs="--model_name $candidate_model_name --n_workers 4 --temperature $temp --generation_strategy sampling --num_return_sequences 10"
+kwargs="--model_name $candidate_model_name --max_length=4096 --temperature $temp --generation_strategy sampling --num_return_sequences 10"
 script="python generate_patches.py $samples_path $patch_strategy $kwargs"
 apptainer exec --nv $container_name $script
 # echo $script
